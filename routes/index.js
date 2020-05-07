de5c9a8734dadb9b8de5747f72cf3bd6eedf84d0
jiayichaonb <60302391+jiayichaonb@users.noreply.github.com>
2020-05-07 05:05:52 -0400

    Modify the html file as hadlebar form
    Simply add some route "login.js" "signup.js" "main.js"

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,14 @@
+
+
 const path = require('path')
 const mainRoutes = require('./main')
+const loginRoutes = require('./login')
+const signupRoutes = require('./signup')
 
 const constructor = (app) => {
-    app.use('/', mainRoutes)
+    app.use('/', mainRoutes);
+    app.use('/login', loginRoutes);
+    app.use('/signup', signupRoutes);
 }
 
 module.exports = constructor

5f06e3798a26416ee675e4e4d0b27f4bb7820a94
Ahmet <ahmet@localhost.localdomain>
2020-05-13 14:53:34 -0400

    filename

--- routes/new.js
+++ routes/new.js
@@ -6,7 +6,7 @@ const postData = data.post
 // render create post page only if logged in (check cookie), if not logged in redirect to login
 router.get('/', async (req, res) => {
 	if (req.session.user) { // add checking cookie expiration
-        res.render('createpost')
+        res.render('createPost')
     }
     else {
         res.status(403);
